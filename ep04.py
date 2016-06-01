f6a12051c104bcf287c501ac494146cb362131f4
Bruno Boaventura Scholl <brunobbs@prolog.linux.ime.usp.br>
2016-06-01 16:59:31 -0300

    adiciona verificacao de vizverificaccao de vizinhos e printa row

--- ep04.py
+++ ep04.py
@@ -14,10 +14,28 @@ def leEntrada(nome):
 
 def simulaQuad(n,m,lista,t):
     tab =  [[0 for k in range(m)] for l in range(n)]
+    tab2 =  [[0 for c in range(m)] for d in range(n)]
     for x in range(len(lista)):
         clinha = n - lista[x][0]
         ccoluna = lista[x][1]
         tab[clinha][ccoluna] = 1
+    for vezes in range(t):
+        for i in range(n):
+            for j in range(m):
+
+                somViz = tab[i-1][j-1] + tab[i-1][j] + tab[i-1][j+1] + tab[i][j-1] + tab[i][j+1] + tab[i+1][j-1] + tab[i+1][j+1]
+
+                if (somViz == 3):
+
+                    tab2[i][j] = 1
+
+                elif tab[i][j] == 1 and somViz == 2 :
+
+                    tab2[i][j] = 1
+
+    for row in tab2:
+    for val in row:
+        print '{:4}'.format(val),
+    print
         
-    i=1
 
