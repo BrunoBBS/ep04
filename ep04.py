f1fd7c85983fb626845647eb036445f77414d48b
Bruno BBS <bruno.bbs3@hotmail.com>
2016-06-01 19:10:02 -0300

    caga tudo com o cod do lana

--- ep04.py
+++ ep04.py
@@ -15,17 +15,46 @@ def leEntrada(nome):
     return modo,listaVivos
 
 def simulaQuad(n,m,lista,t):
-    tab =  [[0 for k in range(m)] for l in range(n)]
+    M = []                     
+    for i in range(n+2):       
+        M.append([])           
+        for j in range(m+2):
+            M[i].append(0)  
+
+
     for x in range(len(lista)):
-        clinha = (n-1) - lista[x][0]
-        ccoluna = lista[x][1]
+        clinha = (n-1) - lista[x][0] + 1
+        ccoluna = lista[x][1] + 1
         tab[clinha][ccoluna] = 1
+    
+    for k in range(t):
+        Mp = []
+        for i in range(n+2):
+            Mp.append([])
+            for j in range(m+2):
+                Mp[i].append(0)
+
+        M[0][0] =  M[n][m]
+        M[n+1][0] =  M[1][m]
+        M[0][m+1] =  M[n][1]
+        M[n+1][m+1] = M[1][1]
+        
+        for i in range(1, n+1, 1): 
+            M[i][0] = M[i][m]
+            M[i][m+1] = M[i][1]
+
+        for i in range(1, m+1, 1): 
+            M[0][i] = M[n][i]
+            M[n+1][i] = M[1][i]
+
+
+    
     for vezes in range(t):
         tab2 =  [[0 for c in range(m)] for d in range(n)]
         for i in range(n):
             for j in range(m):
 
-                somViz = tab[i-1][j-1] + tab[i-1][j] + tab[i-1][((j+1)%(m-1))-1] + tab[i][j-1] + tab[i][((j+1)%(m-1))-1] + tab[((i+1)%(n-1))-1][j-1] + tab[((i+1)%(n-1))-1][j] +tab[((i+1)%(n-1))-1][((j+1)%(m-1))-1]
+                tab[i-1][j-1] + tab[i-1][j] + tab[i-1][((j+1)%(m-1))-1] + tab[i][j-1] + tab[i][((j+1)%(m-1))-1] + tab[((i+1)%(n-1))-1][j-1] + tab[((i+1)%(n-1))-1][j] +tab[((i+1)%(n-1))-1][((j+1)%(m-1))-1]
                 print(tab[i-1][j-1])
                 print(tab[i-1][j])
                 print(tab[i-1][((j+1)%(m-1))-1])
