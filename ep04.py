602244f7f8cd1571f066a357498929c65a49f2a1
Bruno Boaventura Scholl <brunobbs@prolog.linux.ime.usp.br>
2016-06-01 17:41:44 -0300

    varias mod

--- ep04.py
+++ ep04.py
@@ -18,14 +18,14 @@ def simulaQuad(n,m,lista,t):
     tab =  [[0 for k in range(m)] for l in range(n)]
     tab2 =  [[0 for c in range(m)] for d in range(n)]
     for x in range(len(lista)):
-        clinha = n - lista[x][0]
+        clinha = (n-1) - lista[x][0]
         ccoluna = lista[x][1]
         tab[clinha][ccoluna] = 1
     for vezes in range(t):
         for i in range(n):
             for j in range(m):
 
-                somViz = tab[i-1][j-1] + tab[i-1][j] + tab[i-1][j+1] + tab[i][j-1] + tab[i][j+1] + tab[i+1][j-1] + tab[i+1][j+1]
+                somViz = tab[i-1][j-1] + tab[i-1][j] + tab[i-1][(j+1)%(m-1)-1] + tab[i][j-1] + tab[i][(j+1)%(m-1)-1] + tab[(i+1)%(n-1)-1][j-1] + tab[(i+1)%(n-1)-1][(j+1)%(m-1)-1]
 
                 if (somViz == 3):
 
@@ -34,14 +34,16 @@ def simulaQuad(n,m,lista,t):
                 elif tab[i][j] == 1 and somViz == 2 :
 
                     tab2[i][j] = 1
-    print(numpy.matrix(tab2))
+
+                tab = tab2
+    print(numpy.matrix(tab))
         
 def main():
     m = int(input('colunas: '))
     n = int(input('linhas: '))
     t = int(input('vezes: '))
-    modo, listaVivos = leEntrada('teste.txt')
-    if modo == 'Q':
+    modo,listaVivos = leEntrada('teste.txt')
+    if modo == 0:
         simulaQuad(n,m,listaVivos,t)
 
 main()
